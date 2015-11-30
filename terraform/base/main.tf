39a1abe66f9b4d14e0d373686326112e12d2450d
KensoDev <avi@kensodev.com>
2015-11-30 13:16:45 -0800

    Adding configuration to the terraform cluster config

--- terraform/base/main.tf
+++ terraform/base/main.tf
@@ -53,7 +53,7 @@ resource "aws_security_group" "cluster" {
         from_port = 0
         to_port = 65535
         protocol = "tcp"
-				self = true
+        self = true
     }
 
     egress {
@@ -79,6 +79,25 @@ resource "aws_security_group" "chef" {
 resource "aws_security_group" "logger" {
     name = "logger"
     description = "Logger Security Group"
+
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.cluster.id}"]
+    }
+
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "udp"
+        security_groups = ["${aws_security_group.cluster.id}"]
+    }
+}
+
+resource "aws_security_group" "stats" {
+    name = "stats"
+    description = "Stats Security Group"
 
     ingress {
         from_port = 0
@@ -152,6 +171,7 @@ resource "aws_instance" "sensu" {
 
   user_data = "${file(\"sensu-userdata.yml\")}"
 }
+
 resource "aws_instance" "logger" {
   instance_type = "m4.large"
   ami = "${lookup(var.aws_amis, var.aws_region)}"
@@ -163,4 +183,30 @@ resource "aws_instance" "logger" {
   }
 
   user_data = "${file(\"logger-userdata.yml\")}"
+}
+
+resource "aws_instance" "stats" {
+  instance_type = "m4.large"
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+  key_name = "${var.key_name}"
+  security_groups = ["${aws_security_group.cluster.name}", "${aws_security_group.external_connections.name}", "${aws_security_group.stats.name}"]
+
+  tags {
+      Name = "stats"
+  }
+
+  user_data = "${file(\"stats-userdata.yml\")}"
+}
+
+resource "aws_instance" "sensu" {
+  instance_type = "m4.large"
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+  key_name = "${var.key_name}"
+  security_groups = ["${aws_security_group.cluster.name}", "${aws_security_group.external_connections.name}"]
+
+  tags {
+      Name = "sensu"
+  }
+
+  user_data = "${file(\"sensu-userdata.yml\")}"
 }
