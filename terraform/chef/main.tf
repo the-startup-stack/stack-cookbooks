a46ef6cbf40b7e13e536e5d3f314c194d32d7226
KensoDev <avi@kensodev.com>
2016-01-29 07:54:21 -0800

    Refactoring terraform for chef

--- terraform/chef/main.tf
+++ terraform/chef/main.tf
@@ -1,12 +1,12 @@
-module "web_base" {
-  source = "../web_base"
+module "base" {
+  source = "../base"
   your_ip_address = "${var.your_ip_address}"
 }
 
 resource "aws_security_group" "chef" {
     name        = "chef"
     description = "Chef Security Group"
-    vpc_id      = "${module.web_base.default_vpc_id}"
+    vpc_id      = "${module.base.default_vpc_id}"
 
     egress {
         from_port   = 0
@@ -20,8 +20,8 @@ resource "aws_instance" "chef" {
   instance_type          = "m3.large"
   ami                    = "${lookup(var.aws_amis, var.aws_region)}"
   key_name               = "${var.key_name}"
-  subnet_id              = "${module.web_base.default_subnet_id}"
-  vpc_security_group_ids = ["${module.web_base.default_security_group_id}", "${module.web_base.external_connections_security_group_id}"]
+  subnet_id              = "${module.base.default_subnet_id}"
+  vpc_security_group_ids = ["${module.base.default_security_group_id}", "${module.base.external_connections_security_group_id}"]
 
   tags {
       Name = "chef"
