c34b55418765ba12282f0b5a66662c006ebb97eb
KensoDev <avi@kensodev.com>
2016-02-15 13:48:42 -0800

    Adding a security group for marathon

--- terraform/cluster/main.tf
+++ terraform/cluster/main.tf
@@ -3,6 +3,19 @@ module "base" {
   your_ip_address = "${var.your_ip_address}"
 }
 
+resource "aws_security_group" "marathon" {
+    name        = "chef"
+    description = "Chef Security Group"
+    vpc_id      = "${module.base.default_vpc_id}"
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_instance" "marathon" {
   instance_type   = "m4.large"
   ami             = "${lookup(var.aws_amis, var.aws_region)}"
